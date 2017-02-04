e02f4cf4ce8ebb3e1e374d4304d0c6f98644b406
Jesus Flores <areyalp@gmail.com>
2017-02-04 17:50:54 -0400

    Added some changes to TimerCycle example

--- SoftparkMulti/src/softparkmulti/TimerCycle.java
+++ SoftparkMulti/src/softparkmulti/TimerCycle.java
@@ -11,15 +11,22 @@ public class TimerCycle {
         timer = new Timer();
         timer.scheduleAtFixedRate(new RemindTask(),
 	               0,        //initial delay
-	               1*1000);  //subsequent rate
+	               1000);  //subsequent rate
+    }
+    
+    public static void main(String[] args) {
+    	new TimerCycle();
     }
 
     class RemindTask extends TimerTask {
-	int numWarningBeeps = 0;
+    	int numWarningBeeps = 0;
 
-        public void run() {
+        public synchronized void run() {
         	numWarningBeeps++;
         	System.out.format("Beep! " + numWarningBeeps + "%n");
+        	if (numWarningBeeps > 5) {
+        		timer.cancel();
+        	}
         }
     }
 	
